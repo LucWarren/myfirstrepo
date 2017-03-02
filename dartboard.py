c859fcd39550c45f8266f029f83928e4e2baa9fc
LucWarren <lucwarrendonnolly.ld@gmail.com>
2017-03-02 13:32:13 +0200

    i added the dartboard project

---
+++ dartboard.py
@@ -0,0 +1,23 @@
+import turtle
+import math
+wn = turtle.Screen()
+luc = turtle.Turtle()
+luc.speed(100000)
+distance = 6
+posY = -260
+posX = 0
+def  circle():
+    for i in range(360):
+        luc.fd(distance)
+        luc.lt(1)
+
+
+for i in range(6):
+    luc.pu()
+    luc.goto(posX,posY)
+    luc.pd()
+    circle()
+    distance -= 1
+    posY  += 54
+    
+
